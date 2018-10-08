91fa06337a244d3a63481f4b17f8275fdd942827
Michael J. Lyons <mikelyons7@me.com>
2018-10-08 16:47:33 -0600

    Add getAllWines and getAllVineyards methods

--- routes/requests.js
+++ routes/requests.js
@@ -1,6 +1,32 @@
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[enviroment];
+const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
+
+const getAllVineyards = (request, response) => {
+  database('vineyards')
+    .select()
+    .then(vineyards => {
+      response.status(200).json({
+        status: 'ok',
+        data: vineyards,
+        message: 'Enjoy your vineyards!'
+      });
+    })
+    .catch(error => response.status(500).json({ error }));
+};
+
+const getAllWines = (request, response) => {
+  database('wines')
+    .select()
+    .then(wines => {
+      response.status(200).json({
+        status: 'ok',
+        data: wines,
+        message: 'Enjoy your vitis vinifera!'
+      });
+    })
+    .catch(error => response.status(500).json({ error }));
+};
 
 module.exports = {
   getAllVineyards,
