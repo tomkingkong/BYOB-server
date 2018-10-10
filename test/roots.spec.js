d792df86af71c48a3b699173a2cb8b47dd6f4cac
Tom King <design.tomking@gmail.com>
2018-10-10 15:03:09 -0600

    Fix put happy path for vineyard

--- test/roots.spec.js
+++ test/roots.spec.js
@@ -147,22 +147,23 @@ describe('API ROUTES', () => {
       });
   });
 
-  it.skip('DELETE /api/vi/vineyards/:vineyard_id should remove a vineyard SAD', done => {});
-
-  it('GET /api/v1/vineyards should return all vineyards HAPPY', done => {
+  it('PUT /api/v1/vineyards/:vineyard_id should update one vineyard HAPPY', done => {
     chai
       .request(server)
-      .get('/api/v1/vineyards')
+      .put('/api/v1/vineyards/1')
+      .send({
+        name: `FunkyTown Vineyards`,
+        location: `Vail, CO`,
+        date_established: 2000,
+        harvest: false
+      })
       .end((err, response) => {
         response.should.have.status(200);
         response.should.be.json;
-        response.body.should.be.a('object');
         response.body.status.should.be.a('string');
         response.body.status.should.equal('ok');
-        response.body.message.should.be.a('string');
-        response.body.message.should.equal('Enjoy your vineyards!');
         response.body.data.should.be.a('array');
-        response.body.data.length.should.equal(3);
+        response.body.data.length.should.equal(1);
         response.body.data[0].should.have.property('name');
         response.body.data[0].name.should.equal('FunkyTown Vineyards');
         response.body.data[0].should.have.property('location');
@@ -170,7 +171,11 @@ describe('API ROUTES', () => {
         response.body.data[0].should.have.property('date_established');
         response.body.data[0].date_established.should.equal(2000);
         response.body.data[0].should.have.property('harvest');
-        response.body.data[0].harvest.should.equal(true);
+        response.body.data[0].harvest.should.equal(false);
+        done();
+      });
+  });
+
         done();
       });
   });
